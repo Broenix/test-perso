43f49527e0995cb1d9d0df7e9d7cdb260f3e807f
theo chevalier <chevalie.theo@gmail.com>
2023-01-26 22:19:36 +0100

    On essaie des trucs

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :jeuxvideo, only: [:index]
+  resources :serie, only: [:index]
+  resources :film, only: [:index]
 end
